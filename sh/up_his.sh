e15c1217e15687cb4b920807ea8fb3c321c55941
卢松林 <2445859460@qq.com>
2020-11-25 17:50:54 +0800

    update sh

--- sh/up_his.sh
+++ sh/up_his.sh
@@ -2,5 +2,4 @@
 
 source /home/klein/workspace/nCOV/bin/activate env_nCov
 python3 /home/klein/workspace/nCOV/spider.py up_his >> /home/klein/workspace/nCOV/resource/log/log_his
-~
-~
+
--- sh/up_word.sh
+++ sh/up_word.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/klein/workspace/nCOV/bin/activate env_nCov  #env_nCov是conda创建的虚拟环境
+source /Anaconda/ls/envs/env_nCov/bin/activate env_nCov  #env_nCov是conda创建的虚拟环境
 python3 /home/klein/workspace/nCOV/spider.py up_word >> /home/klein/workspace/nCOV/resource/log/log_word
